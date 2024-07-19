ef043d57fa1671e2a4a3784da86e90b6554620ac
clement-ux <clemmoller@gmail.com>
2024-07-19 16:23:47 +0200

    feat: improve deployment script.

--- script/001_OETH_ARM.s.sol
+++ script/001_OETH_ARM.s.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.23;
 
 // Foundry
-import {Test} from "forge-std/Test.sol";
 import {Script} from "forge-std/Script.sol";
 
 // Contracts
@@ -12,12 +11,28 @@ import {OEthARM} from "contracts/OethARM.sol";
 // Utils
 import {Mainnet} from "test/utils/Addresses.sol";
 
-contract OETHARMScript is Script {
-    // Fetch PK from env and derive the deployer address
-    address public deployer = vm.addr(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+/// @notice Deploy the OEthARM contract using a proxy.
+/// @dev 1. Deploy the proxy contract.
+///      2. Deploy the OEthARM implementation contract.
+///      3. Initialize the proxy contract with the OEthARM implementation contract.
+contract _001_OETHARMScript is Script {
+    address public deployer;
 
     //////////////////////////////////////////////////////
     /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public {
+        if (vm.envExists("DEPLOYER_PRIVATE_KEY")) {
+            // Fetch PK from env and derive the deployer address
+            deployer = vm.addr(vm.envUint("DEPLOYER_PRIVATE_KEY"));
+        } else {
+            // If no PK is provided, use a default deployer address
+            deployer = makeAddr("deployer");
+        }
+    }
+
+    //////////////////////////////////////////////////////
+    /// --- RUN
     //////////////////////////////////////////////////////
     function run() public {
         // 🟡 All the next transactions will be sent by the deployer if `--broadcast`option is used on the command line 🟡
