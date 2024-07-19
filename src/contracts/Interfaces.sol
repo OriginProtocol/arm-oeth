92997d5df9294376bbe933eea9d4516faa3becb7
clement-ux <clemmoller@gmail.com>
2024-07-19 14:36:51 +0200

    forge fmt

--- src/contracts/Interfaces.sol
+++ src/contracts/Interfaces.sol
@@ -9,6 +9,7 @@ interface IERC20 {
     function approve(address spender, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
     function decimals() external view returns (uint8);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
