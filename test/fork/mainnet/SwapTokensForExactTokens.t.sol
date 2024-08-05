8f3f5fe3fcb8fb6df3ccf8dbc73b2fdc4a7d01f3
clement-ux <clemmoller@gmail.com>
2024-08-05 15:37:38 +0200

    test: fix missing approvals.

--- test/fork/mainnet/SwapTokensForExactTokens.t.sol
+++ test/fork/mainnet/SwapTokensForExactTokens.t.sol
@@ -19,6 +19,9 @@ contract Fork_Concrete_OethARM_SwapTokensForExactTokens_Test_ is Fork_Shared_Tes
         deal(address(oeth), address(this), 100 ether);
         deal(address(weth), address(oethARM), 100 ether);
         deal(address(oeth), address(oethARM), 100 ether);
+
+        // Approve tokens
+        oeth.approve(address(oethARM), type(uint256).max);
     }
 
     //////////////////////////////////////////////////////
