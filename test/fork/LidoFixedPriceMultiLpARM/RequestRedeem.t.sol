b2c98cbdec2a6a72cce408f6af616b3e54f2b92f
Clément <clemmoller@gmail.com>
2024-09-22 11:59:08 +0200

    test: add failing test.

--- test/fork/LidoFixedPriceMultiLpARM/RequestRedeem.t.sol
+++ test/fork/LidoFixedPriceMultiLpARM/RequestRedeem.t.sol
@@ -10,6 +10,8 @@ contract Fork_Concrete_LidoFixedPriceMultiLpARM_RequestRedeem_Test_ is Fork_Shar
     //////////////////////////////////////////////////////
     function setUp() public override {
         super.setUp();
+
+        deal(address(weth), address(this), 1_000 ether);
     }
 
     //////////////////////////////////////////////////////
@@ -24,5 +26,30 @@ contract Fork_Concrete_LidoFixedPriceMultiLpARM_RequestRedeem_Test_ is Fork_Shar
 
         lidoFixedPriceMulltiLpARM.deposit(10 ether);
         lidoFixedPriceMulltiLpARM.requestRedeem(8 ether);
+    }
+
+    /// @notice Test the `requestRedeem` function when there are no profits and the first deposit is made.
+    function test_RequestWithdraw_AfterFirstDeposit_NoPerfs_EmptyWithdrawQueue()
+        public
+        setTotalAssetsCap(DEFAULT_AMOUNT + MIN_TOTAL_SUPPLY)
+        setLiquidityProviderCap(address(this), DEFAULT_AMOUNT)
+        depositInLidoFixedPriceMultiLpARM(address(this), DEFAULT_AMOUNT)
+    {
+        // Assertions Before
+        assertEq(steth.balanceOf(address(lidoFixedPriceMulltiLpARM)), 0);
+        assertEq(weth.balanceOf(address(lidoFixedPriceMulltiLpARM)), MIN_TOTAL_SUPPLY + DEFAULT_AMOUNT);
+        assertEq(lidoFixedPriceMulltiLpARM.outstandingEther(), 0);
+        assertEq(lidoFixedPriceMulltiLpARM.feesAccrued(), 0); // No perfs so no fees
+        assertEq(lidoFixedPriceMulltiLpARM.lastTotalAssets(), MIN_TOTAL_SUPPLY + DEFAULT_AMOUNT);
+        assertEq(lidoFixedPriceMulltiLpARM.balanceOf(address(this)), DEFAULT_AMOUNT);
+        assertEq(lidoFixedPriceMulltiLpARM.totalSupply(), MIN_TOTAL_SUPPLY + DEFAULT_AMOUNT);
+        assertEq(liquidityProviderController.liquidityProviderCaps(address(this)), 0);
+        assertEqQueueMetadata(0, 0, 0, 0);
+
+        // Main Call
+        (uint256 requestId, uint256 assets) = lidoFixedPriceMulltiLpARM.requestRedeem(DEFAULT_AMOUNT);
+
+        assertEq(requestId, 0); // First request
+        assertEq(assets, DEFAULT_AMOUNT, "Wrong amount of assets"); // As no profits, assets returned are the same as deposited
     }
 }
