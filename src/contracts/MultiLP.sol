bc0403e9116d05d4cabd07ccbb94f279fd86e5a0
Nicholas Addison <nick@addisonbrown.com.au>
2024-09-23 19:47:28 +1000

    Fix requestRedeem which now stored the updated queued amount

--- src/contracts/MultiLP.sol
+++ src/contracts/MultiLP.sol
@@ -130,6 +130,8 @@ abstract contract MultiLP is AbstractARM, ERC20Upgradeable {
 
         // Store the next withdrawal request
         withdrawalQueueMetadata.nextWithdrawalIndex = SafeCast.toUint128(requestId + 1);
+        // Store the updated queued amount which reserves WETH in the withdrawal queue
+        withdrawalQueueMetadata.queued = SafeCast.toUint128(queued);
         // Store requests
         withdrawalRequests[requestId] = WithdrawalRequest({
             withdrawer: msg.sender,
