45b3bf0695395feaec34a77abc922df603b6a7a5
Nicholas Addison <nick@addisonbrown.com.au>
2024-10-04 13:21:20 +1000

    Collapsed LidoLiquidityManager into LidoARM

--- src/contracts/LidoARM.sol
+++ src/contracts/LidoARM.sol
@@ -5,7 +5,7 @@ import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Ini
 import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
 import {AbstractARM} from "./AbstractARM.sol";
-import {LidoLiquidityManager} from "./LidoLiquidityManager.sol";
+import {IERC20, IStETHWithdrawal, IWETH} from "./Interfaces.sol";
 
 /**
  * @title Lido (stETH) Automated Redemption Manager (ARM)
@@ -15,14 +15,24 @@ import {LidoLiquidityManager} from "./LidoLiquidityManager.sol";
  * A performance fee is also collected on increases in the ARM's total assets.
  * @author Origin Protocol Inc
  */
-contract LidoARM is Initializable, AbstractARM, LidoLiquidityManager {
+contract LidoARM is Initializable, AbstractARM {
+    /// @notice The address of the Lido stETH token
+    IERC20 public immutable steth;
+    /// @notice The address of the Wrapped ETH (WETH) token
+    IWETH public immutable weth;
+    /// @notice The address of the Lido Withdrawal Queue contract
+    IStETHWithdrawal public immutable withdrawalQueue;
+
+    uint256 public outstandingEther;
+
     /// @param _steth The address of the stETH token
     /// @param _weth The address of the WETH token
     /// @param _lidoWithdrawalQueue The address of the Lido's withdrawal queue contract
-    constructor(address _steth, address _weth, address _lidoWithdrawalQueue)
-        AbstractARM(_weth, _steth, _weth)
-        LidoLiquidityManager(_steth, _weth, _lidoWithdrawalQueue)
-    {}
+    constructor(address _steth, address _weth, address _lidoWithdrawalQueue) AbstractARM(_weth, _steth, _weth) {
+        steth = IERC20(_steth);
+        weth = IWETH(_weth);
+        withdrawalQueue = IStETHWithdrawal(_lidoWithdrawalQueue);
+    }
 
     /// @notice Initialize the storage variables stored in the proxy contract.
     /// The deployer that calls initialize has to approve the this ARM's proxy contract to transfer 1e12 WETH.
@@ -43,7 +53,9 @@ contract LidoARM is Initializable, AbstractARM, LidoLiquidityManager {
         address _liquidityProviderController
     ) external initializer {
         _initARM(_operator, _name, _symbol, _fee, _feeCollector, _liquidityProviderController);
-        _initLidoLiquidityManager();
+
+        // Approve the Lido withdrawal queue contract. Used for redemption requests.
+        steth.approve(address(withdrawalQueue), type(uint256).max);
     }
 
     /**
@@ -60,9 +72,55 @@ contract LidoARM is Initializable, AbstractARM, LidoLiquidityManager {
     }
 
     /**
+     * @notice Request a stETH for ETH withdrawal.
+     * Reference: https://docs.lido.fi/contracts/withdrawal-queue-erc721/
+     * Note: There is a 1k amount limit. Caller should split large withdrawals in chunks of less or equal to 1k each.)
+     */
+    function requestStETHWithdrawalForETH(uint256[] memory amounts)
+        external
+        onlyOperatorOrOwner
+        returns (uint256[] memory requestIds)
+    {
+        requestIds = withdrawalQueue.requestWithdrawals(amounts, address(this));
+
+        // Sum the total amount of stETH being withdraw
+        uint256 totalAmountRequested = 0;
+        for (uint256 i = 0; i < amounts.length; i++) {
+            totalAmountRequested += amounts[i];
+        }
+
+        // Increase the Ether outstanding from the Lido Withdrawal Queue
+        outstandingEther += totalAmountRequested;
+    }
+
+    /**
+     * @notice Claim the ETH owed from the redemption requests and convert it to WETH.
+     * Before calling this method, caller should check on the request NFTs to ensure the withdrawal was processed.
+     */
+    function claimStETHWithdrawalForWETH(uint256[] memory requestIds) external onlyOperatorOrOwner {
+        uint256 etherBefore = address(this).balance;
+
+        // Claim the NFTs for ETH.
+        uint256 lastIndex = withdrawalQueue.getLastCheckpointIndex();
+        uint256[] memory hintIds = withdrawalQueue.findCheckpointHints(requestIds, 1, lastIndex);
+        withdrawalQueue.claimWithdrawals(requestIds, hintIds);
+
+        uint256 etherAfter = address(this).balance;
+
+        // Reduce the Ether outstanding from the Lido Withdrawal Queue
+        outstandingEther -= etherAfter - etherBefore;
+
+        // Wrap all the received ETH to WETH.
+        weth.deposit{value: etherAfter}();
+    }
+
+    /**
      * @dev Calculates the amount of stETH in the Lido Withdrawal Queue.
      */
-    function _externalWithdrawQueue() internal view override(AbstractARM, LidoLiquidityManager) returns (uint256) {
-        return LidoLiquidityManager._externalWithdrawQueue();
+    function _externalWithdrawQueue() internal view override returns (uint256) {
+        return outstandingEther;
     }
+
+    // This method is necessary for receiving the ETH claimed as part of the withdrawal.
+    receive() external payable {}
 }
