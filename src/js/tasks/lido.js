f855b8651befa234f4c22f6565491a3d858b5590
Nicholas Addison <nick@addisonbrown.com.au>
2024-10-24 13:45:08 +1100

    Added minSellPrice and maxBuyPrice to setPrices hardhat task setPrice Hardhat task can now be done off Curve pool

--- src/js/tasks/lido.js
+++ src/js/tasks/lido.js
@@ -17,28 +17,45 @@ const {
   parseDeployedAddress,
 } = require("../utils/addressParser");
 const { resolveAddress, resolveAsset } = require("../utils/assets");
+const { getCurvePrices } = require("../utils/curve");
 
 const log = require("../utils/logger")("task:lido");
 
 const setPrices = async (options) => {
-  const { signer, arm, fee, tolerance, buyPrice, midPrice, sellPrice, inch } =
-    options;
+  const {
+    signer,
+    arm,
+    fee,
+    tolerance,
+    buyPrice,
+    midPrice,
+    sellPrice,
+    minSellPrice,
+    maxBuyPrice,
+    curve,
+    inch,
+  } = options;
 
   // get current ARM stETH/WETH prices
-  const currentTradeRate0 = parseUnits("1", 72) / (await arm.traderate0());
-  const currentTradeRate1 = await arm.traderate1();
-  log(`current sell price : ${formatUnits(currentTradeRate0, 36)}`);
-  log(`current buy price  : ${formatUnits(currentTradeRate1, 36)}`);
+  const currentSellPrice = parseUnits("1", 72) / (await arm.traderate0());
+  const currentBuyPrice = await arm.traderate1();
+  log(`current sell price : ${formatUnits(currentSellPrice, 36)}`);
+  log(`current buy price  : ${formatUnits(currentBuyPrice, 36)}`);
 
   let targetSellPrice;
   let targetBuyPrice;
-  if (!buyPrice && !sellPrice && (midPrice || inch)) {
+  if (!buyPrice && !sellPrice && (midPrice || curve || inch)) {
     // get latest 1inch prices if no midPrice is provided
     const referencePrices = midPrice
       ? {
           midPrice: parseUnits(midPrice.toString(), 18),
         }
-      : await get1InchPrices(options.amount);
+      : inch
+      ? await get1InchPrices(options.amount)
+      : await getCurvePrices({
+          ...options,
+          poolAddress: addresses.mainnet.CurveStEthPool,
+        });
     log(`mid price          : ${formatUnits(referencePrices.midPrice)}`);
 
     const FeeScale = BigInt(1e6);
@@ -50,6 +67,53 @@ const setPrices = async (options) => {
       (referencePrices.midPrice * BigInt(1e18) * FeeScale) / feeRate;
     targetBuyPrice =
       (referencePrices.midPrice * BigInt(1e18) * feeRate) / FeeScale;
+
+    const minSellPriceBN = parseUnits(minSellPrice.toString(), 36);
+    const maxBuyPriceBN = parseUnits(maxBuyPrice.toString(), 36);
+    if (targetSellPrice < minSellPriceBN) {
+      log(
+        `target sell price ${formatUnits(
+          targetSellPrice,
+          36
+        )} is below min sell price ${minSellPrice} so will use min`
+      );
+      targetSellPrice = minSellPriceBN;
+    }
+    if (targetBuyPrice > maxBuyPriceBN) {
+      log(
+        `target buy price ${formatUnits(
+          targetBuyPrice,
+          36
+        )} is above max buy price ${maxBuyPrice} so will use max`
+      );
+      targetBuyPrice = maxBuyPriceBN;
+    }
+
+    const crossPrice = await arm.crossPrice();
+    if (targetSellPrice < crossPrice) {
+      log(
+        `target sell price ${formatUnits(
+          targetSellPrice,
+          36
+        )} is below cross price ${formatUnits(
+          crossPrice,
+          36
+        )} so will use cross price`
+      );
+      targetSellPrice = crossPrice;
+    }
+    if (targetBuyPrice >= crossPrice) {
+      log(
+        `target buy price ${formatUnits(
+          targetBuyPrice,
+          36
+        )} is above cross price ${formatUnits(
+          crossPrice,
+          36
+        )} so will use cross price`
+      );
+      targetBuyPrice = crossPrice - 1n;
+    }
   } else if (buyPrice && sellPrice) {
     targetSellPrice = parseUnits(sellPrice.toString(), 18) * BigInt(1e18);
     targetBuyPrice = parseUnits(buyPrice.toString(), 18) * BigInt(1e18);
@@ -62,7 +126,9 @@ const setPrices = async (options) => {
   log(`target sell price  : ${formatUnits(targetSellPrice, 36)}`);
   log(`target buy  price  : ${formatUnits(targetBuyPrice, 36)}`);
 
-  const diffBuyPrice = abs(targetBuyPrice - currentTradeRate1);
+  const diffSellPrice = abs(targetSellPrice - currentSellPrice);
+  log(`sell price diff     : ${formatUnits(diffSellPrice, 36)}`);
+  const diffBuyPrice = abs(targetBuyPrice - currentBuyPrice);
   log(`buy price diff     : ${formatUnits(diffBuyPrice, 36)}`);
 
   // tolerance option is in basis points
@@ -70,9 +136,7 @@ const setPrices = async (options) => {
   log(`tolerance          : ${formatUnits(toleranceScaled, 36)}`);
 
   // decide if rates need to be updated
-  if (diffBuyPrice > toleranceScaled) {
-    // Note the prices of setPrices is from the AMM perspective and not the Trader
-    // hence the buy and sell prices are swapped
+  if (diffSellPrice > toleranceScaled || diffBuyPrice > toleranceScaled) {
     console.log(`About to update ARM prices`);
     console.log(`sell: ${formatUnits(targetSellPrice, 36)}`);
     console.log(`buy : ${formatUnits(targetBuyPrice, 36)}`);
@@ -84,10 +148,13 @@ const setPrices = async (options) => {
     await logTxDetails(tx, "setPrices", options.confirm);
   } else {
     console.log(
-      `No price update as price diff of ${formatUnits(
+      `No price update as price diff of buy ${formatUnits(
         diffBuyPrice,
-        36
-      )} < tolerance ${formatUnits(toleranceScaled, 36)}`
+        32
+      )} and sell ${formatUnits(diffSellPrice, 32)} < tolerance ${formatUnits(
+        toleranceScaled,
+        32
+      )} basis points`
     );
   }
 };
