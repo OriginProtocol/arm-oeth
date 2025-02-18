29d1ee5191aa23fe9f12818aede3750321218ef9
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2025-02-18 15:27:29 +0530

    Add deployment script to update cross price

---
+++ script/deploy/mainnet/004_UpdateCrossPriceScript.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.23;
+
+import "forge-std/console.sol";
+
+import {GovProposal, GovSixHelper} from "contracts/utils/GovSixHelper.sol";
+import {AbstractDeployScript} from "../AbstractDeployScript.sol";
+
+contract UpdateCrossPriceMainnetScript is AbstractDeployScript {
+    using GovSixHelper for GovProposal;
+
+    GovProposal public govProposal;
+
+    string public constant override DEPLOY_NAME = "004_UpdateCrossPriceScript";
+    bool public constant override proposalExecuted = false;
+
+    function _execute() internal override {}
+
+    function _buildGovernanceProposal() internal override {
+        govProposal.setDescription("Update Cross Price for Lido ARM");
+
+        uint256 newCrossPrice = 0.9999 * 1e36;
+
+        govProposal.action(deployedContracts["LIDO_ARM"], "setCrossPrice(uint256)", abi.encode(newCrossPrice));
+
+        _fork();
+    }
+
+    function _fork() internal override {
+        if (this.isForked()) {
+            govProposal.simulate();
+        }
+    }
+}
