55e0220779b9322658365da47790a3181a3d7d0a
Clément <clemmoller@gmail.com>
2025-04-24 15:19:18 +0200

    refactor: remove unused AggregatorV3Interface from Interfaces.sol

--- src/contracts/Interfaces.sol
+++ src/contracts/Interfaces.sol
@@ -289,24 +289,3 @@ interface IHarvestable {
 interface IMagpieRouter {
     function swapWithMagpieSignature(bytes calldata) external payable returns (uint256 amountOut);
 }
-
-interface AggregatorV3Interface {
-    function decimals() external view returns (uint8);
-
-    function description() external view returns (string memory);
-
-    function version() external view returns (uint256);
-
-    // getRoundData and latestRoundData should both raise "No data present"
-    // if they do not have data to report, instead of returning unset values
-    // which could be misinterpreted as actual reported values.
-    function getRoundData(uint80 _roundId)
-        external
-        view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-    function latestRoundData()
-        external
-        view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
