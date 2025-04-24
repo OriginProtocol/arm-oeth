d6b6c05edec2c87ba45c90b1cf9f7f1c00c0137b
Clément <clemmoller@gmail.com>
2025-04-24 15:06:42 +0200

    refactor: enhance OracleRouter contract with improved comments and code clarity

--- src/contracts/oracle/OracleRouter.sol
+++ src/contracts/oracle/OracleRouter.sol
@@ -1,10 +1,22 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.23;
 
+// Interfaces
 import {AggregatorV3Interface} from "contracts/Interfaces.sol";
 
+// OZ Libraries
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+
+/// @title OracleRouter
+/// @notice This contract is used to fetch the price of an asset from a price feed.
+/// @author Origin Protocol
 contract OracleRouter {
+    using SafeCast for int256;
+
+    /// @notice Address of the fixed price feed, used to indicate a fixed price
     address internal constant FIXED_PRICE = address(0x1);
+
+    /// @notice Maximum allowed staleness buffer above normal Oracle maximum staleness
     uint256 internal constant STALENESS_BUFFER = 1 days;
 
     /// @notice Returns the total price in 18 digit units for a given asset. This implementation
@@ -23,20 +35,27 @@ contract OracleRouter {
         require(updatedAt + maxStaleness >= block.timestamp, "Oracle price too old");
 
         uint8 decimals = AggregatorV3Interface(_feed).decimals();
-        uint256 _price = scaleBy(uint256(_iprice), 18, decimals);
-        return _price;
+        return scaleBy(_iprice.toUint256(), 18, decimals);
     }
 
+    /// @notice Returns the price feed address and maximum staleness for a given asset.
+    /// @param asset address of the asset
+    /// @return feedAddress address of the price feed
+    /// @return maxStaleness maximum staleness for the price feed
     function feedMetadata(address asset) internal view virtual returns (address feedAddress, uint256 maxStaleness) {
         if (asset == 0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38) {
-            // FIXED_PRICE WS/S
+            // FIXED_PRICE: WS/S
             feedAddress = FIXED_PRICE;
-            maxStaleness = 0;
         } else {
             revert("Asset not available");
         }
     }
 
+    /// @notice Scales a number from one decimal representation to another.
+    /// @param x number to scale
+    /// @param to target decimal representation
+    /// @param from source decimal representation
+    /// @return scaled number in target decimal representation
     function scaleBy(uint256 x, uint256 to, uint256 from) internal pure returns (uint256) {
         if (from > to) {
             return x / (10 ** (from - to));
