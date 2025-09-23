cca6293108bf9fb9117775f51fec4f96b5fe581a
Nicholas Addison <nick@addisonbrown.com.au>
2025-09-23 17:59:08 +1000

    Refactor getLendingMarketAPY into a silo utils

--- src/js/tasks/osSiloPrice.js
+++ src/js/tasks/osSiloPrice.js
@@ -1,5 +1,5 @@
 const { formatUnits, parseUnits } = require("ethers");
-const fetch = require("node-fetch");
+const { getLendingMarketAPY } = require("../utils/silo");
 
 const {
   outstandingValidatorWithdrawalRequests,
@@ -97,36 +97,6 @@ const setOSSiloPrice = async (options) => {
 
     await logTxDetails(tx, "setOSSiloPrice");
   }
-};
-
-/**
- * Get the current APY from the ARM's active lending market
- */
-const getLendingMarketAPY = async (siloMarketWrapper) => {
-  if (!siloMarketWrapper) {
-    log("No active lending market found, using default APY of 0%");
-    return 0n;
-  }
-  // Get the underlying Silo market address
-  const underlyingSiloMarket = await siloMarketWrapper.market();
-
-  // TODO: Make sure it can work for non-Silo markets later
-  const response = await fetch(
-    `https://v2.silo.finance/api/detailed-vault/sonic-${underlyingSiloMarket}`,
-  );
-  const data = await response.json();
-
-  // APR scaled to 1e6
-  const apr = Number((1000000n * BigInt(data.supplyApr)) / BigInt(1e18));
-  log(
-    `Current lending APR: ${Number(formatUnits(100n * BigInt(apr), 6)).toFixed(4)}%`,
-  );
-
-  const dailyRate = apr / 365 / 1000000;
-  const apy = Math.pow(1 + dailyRate, 365) - 1;
-
-  // Scale back to 18 decimals
-  return parseUnits(apy.toString(), 18);
 };
 
 /**
