722c29c671212188c1d954bbe481278f7bacb935
Nicholas Addison <nick@addisonbrown.com.au>
2025-09-24 16:17:29 +1000

    collectFees now checks if there is enough liquidity available before trying to collect

--- src/js/tasks/admin.js
+++ src/js/tasks/admin.js
@@ -1,4 +1,5 @@
 const { formatUnits, parseUnits } = require("ethers");
+const erc20Abi = require("../../abis/ERC20.json");
 
 const { logTxDetails } = require("../utils/txLogger");
 
@@ -33,11 +34,36 @@ async function allocate({ arm, signer, threshold }) {
 }
 
 async function collectFees({ arm, signer }) {
+  // Get the amount of fees to be collected
+  const fees = await arm.feesAccrued();
+  const queued = await arm.withdrawsQueued();
+  const claimed = await arm.withdrawsClaimed();
+
+  // Check there is enough liquidity to collect fees
+  const liquidityAssetAddress = await arm.liquidityAsset();
+  const liquidityAsset = new ethers.Contract(
+    liquidityAssetAddress,
+    erc20Abi,
+    signer,
+  );
+  const liquidityBalance = await liquidityAsset.balanceOf(
+    await arm.getAddress(),
+  );
+  const liquidityAvailable = liquidityBalance + claimed - queued;
+  log(`Liquidity available in ARM: ${formatUnits(liquidityAvailable)}`);
+
+  if (fees > liquidityAvailable) {
+    console.log(
+      `Not enough liquidity to collect ${formatUnits(fees)} in fees. The ARM only has ${formatUnits(liquidityAvailable)} available.`,
+    );
+    return;
+  }
+
   // Add 10% buffer to gas limit
   let gasLimit = await arm.connect(signer).collectFees.estimateGas();
   gasLimit = (gasLimit * 11n) / 10n;
 
-  log(`About to collect ARM fees`);
+  log(`About to collect ${formatUnits(fees)} ARM fees`);
   const tx = await arm.connect(signer).collectFees({ gasLimit });
   await logTxDetails(tx, "collectFees");
 }
