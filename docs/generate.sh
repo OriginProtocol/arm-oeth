fcf44a1047ff2b375e4274e8ee0ac2db424785e6
Clément <clemmoller@gmail.com>
2025-10-27 10:27:21 +0100

    generate puml files

--- docs/generate.sh
+++ docs/generate.sh
@@ -14,6 +14,15 @@ sol2uml storage ../src/contracts,../lib -c LidoARM -o LidoARMStorage.svg \
     -st address,address \
     --hideExpand gap,_gap
 
+sol2uml ../src/contracts -v -hv -hf -he -hs -hl -hi -b OriginARM -o OriginARMHierarchy.svg
+sol2uml ../src/contracts -s -d 0 -b OriginARM -o OriginARMSquashed.svg
+sol2uml ../src/contracts -hp -s -d 0 -b OriginARM -o OriginARMPublicSquashed.svg
+sol2uml storage ../src/contracts,../lib -c OriginARM -o OriginARMStorage.svg \
+    -sn eip1967.proxy.implementation,eip1967.proxy.admin \
+    -st address,address \
+    --hideExpand gap,_gap
+
+
 sol2uml ../src/contracts -v -hv -hf -he -hs -hl -hi -b CapManager -o CapManagerHierarchy.svg
 sol2uml ../src/contracts -s -d 0 -b CapManager -o CapManagerSquashed.svg
 
