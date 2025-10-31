6754a15666fa2569dfebfcf570cdda6516728209
Clément <clemmoller@gmail.com>
2025-10-31 20:15:44 +0100

    clean up

--- src/contracts/ARMRouter.sol
+++ src/contracts/ARMRouter.sol
@@ -53,47 +53,13 @@ contract ARMRouter {
     ///                 Swap Functions
     ////////////////////////////////////////////////////
     /// @notice Swaps an exact amount of input tokens for as many output tokens as possible, along the route determined by the path.
-    /// @param amountIn The exact amount of input tokens to swap.
-    /// @param amountOutMin The minimum amount of output tokens that must be received for the transaction not to revert.
-    /// @param path An array of token addresses representing the swap path.
-    /// @param to The address that will receive the output tokens.
-    /// @param deadline The timestamp by which the transaction must be completed.
-    /// @return amounts An array of token amounts for each step in the swap path.
-    function swapExactTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external ensure(deadline) returns (uint256[] memory amounts) {
-        // Get output amounts for the swap
-        amounts = getAmountsOut(amountIn, path);
-        require(amounts[amounts.length - 1] >= amountOutMin, "ARMRouter: INSUFFICIENT_OUTPUT_AMOUNT");
-
-        // Transfer the input tokens from the sender to this contract
-        IERC20(path[0]).transferFrom(msg.sender, address(this), amounts[0]);
-
-        // Perform the swaps along the path
-        uint256 len = path.length;
-        for (uint256 i; i < len - 1; i++) {
-            (address input, address output) = (path[i], path[i + 1]);
-            Config memory config = getConfigFor(input, output);
-
-            AbstractARM(config.addr)
-                .swapExactTokensForTokens(
-                    IERC20(input), IERC20(output), amounts[i], amounts[i + 1], i < len - 2 ? address(this) : to
-                );
-        }
-    }
-
-    /// @notice Swaps an exact amount of input tokens for as many output tokens as possible, along the route determined by the path.
     /// @dev This is a simplified version that handles swaps in a loop without fetching amounts beforehand.
     /// @param amountIn The exact amount of input tokens to swap.
     /// @param amountOutMin The minimum amount of output tokens that must be received for the transaction not to revert.
     /// @param path An array of token addresses representing the swap path.
     /// @param to The address that will receive the output tokens.
     /// @param deadline The timestamp by which the transaction must be completed.
-    function swapExactTokensForTokensSimple(
+    function swapExactTokensForTokens(
         uint256 amountIn,
         uint256 amountOutMin,
         address[] calldata path,
@@ -102,6 +68,9 @@ contract ARMRouter {
     ) external ensure(deadline) returns (uint256[] memory amounts) {
         // Transfer the input tokens from the sender to this contract
         IERC20(path[0]).transferFrom(msg.sender, address(this), amountIn);
+
+        amounts = new uint256[](path.length);
+        amounts[0] = amountIn;
 
         // Perform the swaps along the path
         uint256 len = path.length;
@@ -141,9 +110,8 @@ contract ARMRouter {
     {
         require(path[0] == address(WETH), "ARMRouter: INVALID_PATH");
 
-        // Get output amounts for the swap
-        amounts = getAmountsOut(msg.value, path);
-        require(amounts[amounts.length - 1] >= amountOutMin, "ARMRouter: INSUFFICIENT_OUTPUT_AMOUNT");
+        amounts = new uint256[](path.length);
+        amounts[0] = msg.value;
 
         // Wrap ETH to WETH
         WETH.deposit{value: amounts[0]}();
@@ -151,70 +119,46 @@ contract ARMRouter {
         // Perform the swaps along the path
         uint256 len = path.length;
         for (uint256 i; i < len - 1; i++) {
-            (address input, address output) = (path[i], path[i + 1]);
-            Config memory config = getConfigFor(input, output);
+            address[] memory intermediate = new address[](2);
+            intermediate[0] = path[i];
+            intermediate[1] = path[i + 1];
 
-            AbstractARM(config.addr)
-                .swapExactTokensForTokens(
-                    IERC20(input), IERC20(output), amounts[i], amounts[i + 1], i < len - 2 ? address(this) : to
-                );
+            Config memory config = getConfigFor(intermediate);
+            if (config.sig == bytes4(0)) {
+                uint256[] memory obtained = AbstractARM(config.addr)
+                    .swapExactTokensForTokens(amounts[i], 0, intermediate, i < len - 2 ? address(this) : to, deadline);
+                amounts[i + 1] = obtained[1];
+            } else {
+                (bool success, bytes memory data) = config.addr.call(abi.encodeWithSelector(config.sig, amounts[i]));
+                require(success, "ARMRouter: SWAP_FAILED");
+                amounts[i + 1] = abi.decode(data, (uint256));
+
+                // If this is the last swap, transfer to the recipient
+                if (i == len - 2) IERC20(path[i + 1]).transfer(to, amounts[i + 1]);
+            }
         }
+        require(amounts[amounts.length - 1] >= amountOutMin, "ARMRouter: INSUFFICIENT_OUTPUT_AMOUNT");
     }
 
     ////////////////////////////////////////////////////
     ///                 Helpers Functions
     ////////////////////////////////////////////////////
-    /// @notice Given an input amount of an asset and a swap path, returns the maximum output amounts of each asset in the path.
-    /// @param amountIn The exact amount of input tokens to swap.
-    /// @param path An array of token addresses representing the swap path.
-    /// @return amounts An array of token amounts for each step in the swap path.
-    function getAmountsOut(uint256 amountIn, address[] memory path) internal view returns (uint256[] memory amounts) {
-        uint256 len = path.length;
-        require(len >= 2, "ARMLibrary: INVALID_PATH");
-
-        amounts = new uint256[](len);
-        amounts[0] = amountIn;
-
-        for (uint256 i; i < len - 1; i++) {
-            // Get traderate from ARM
-            uint256 traderate = getTraderate(path[i], path[i + 1]);
-
-            // Calculate output amount based on traderate
-            amounts[i + 1] = (amounts[i] * traderate) / PRICE_SCALE;
-        }
-    }
-
-    /// @notice Given a pair of tokens, returns the current traderate for the swap.
-    /// @param tokenA The address of the first token i.e. the input token.
-    /// @param tokenB The address of the second token i.e. the output token.
-    /// @return traderate The current traderate for the swap.
-    function getTraderate(address tokenA, address tokenB) internal view returns (uint256 traderate) {
-        Config memory config = getConfigFor(tokenA, tokenB);
-        address token0 = address(AbstractARM(config.addr).token0());
-        traderate =
-            tokenA == address(token0) ? AbstractARM(config.addr).traderate0() : AbstractARM(config.addr).traderate1();
-    }
-
-    /// @notice Given a pair of tokens, returns the address of the associated ARM.
-    /// @param tokenA The address of the first token.
-    /// @param tokenB The address of the second token.
-    /// @return arm The address of the associated ARM.
-    function getConfigFor(address tokenA, address tokenB) internal view returns (Config memory arm) {
-        arm = configs[tokenA][tokenB];
-        require(arm.addr != address(0), "ARMRouter: ARM_NOT_FOUND");
-    }
-
     /// @notice Given a pair of tokens, returns the address of the associated ARM.
     /// @param tokenPair An array containing the addresses of the two tokens.
     /// @return arm The address of the associated ARM.
     function getConfigFor(address[] memory tokenPair) internal view returns (Config memory arm) {
-        arm = getConfigFor(tokenPair[0], tokenPair[1]);
+        arm = configs[tokenPair[0]][tokenPair[1]];
+        require(arm.addr != address(0), "ARMRouter: ARM_NOT_FOUND");
     }
 
     ////////////////////////////////////////////////////
     ///                 Owner Functions
     ////////////////////////////////////////////////////
     function registerConfig(address tokenA, address tokenB, bytes4 sig, address armAddress) external {
+        // Max approval for router to interact with ARMs
+        IERC20(tokenA).approve(armAddress, type(uint256).max);
+        IERC20(tokenB).approve(armAddress, type(uint256).max);
+
         configs[tokenA][tokenB] = Config({sig: sig, addr: armAddress});
     }
 }
