baeb1c9cca74a542feba550c4b1d4dbfe251523b
Clément <clemmoller@gmail.com>
2025-11-03 12:21:22 +0100

    Add fork test for Router

---
+++ test/fork/RouterARM/Shared.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+// Test
+import {Base_Test_} from "test/Base.sol";
+
+import {Mainnet} from "contracts/utils/Addresses.sol";
+
+// Contracts
+import {WETH} from "@solmate/tokens/WETH.sol";
+import {Proxy} from "contracts/Proxy.sol";
+import {LidoARM} from "contracts/LidoARM.sol";
+import {LidoARM} from "contracts/LidoARM.sol";
+import {ARMRouter} from "contracts/ARMRouter.sol";
+import {EtherFiARM} from "contracts/EtherFiARM.sol";
+
+// Interfaces
+import {IERC20} from "contracts/Interfaces.sol";
+
+abstract contract Fork_Shared_ARMRouter_Test is Base_Test_ {
+    //////////////////////////////////////////////////////
+    /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public virtual override {
+        // Create and select fork
+        _createAndSelectFork();
+
+        // Generate addresses
+        _generateAddresses();
+
+        // Deploy contracts
+        _deployContracts();
+
+        // Fund contracts
+        _fundContracts();
+
+        // Label contracts
+        labelAll();
+    }
+
+    function _createAndSelectFork() internal {
+        // Check if the PROVIDER_URL is set.
+        require(vm.envExists("PROVIDER_URL"), "PROVIDER_URL not set");
+
+        // Create and select a fork.
+        if (vm.envExists("FORK_BLOCK_NUMBER_MAINNET")) {
+            vm.createSelectFork("mainnet", vm.envUint("FORK_BLOCK_NUMBER_MAINNET"));
+        } else {
+            vm.createSelectFork("mainnet");
+        }
+    }
+
+    function _generateAddresses() internal {
+        // Contracts.
+        weth = IERC20(Mainnet.WETH);
+        eeth = IERC20(Mainnet.EETH);
+        weeth = IERC20(Mainnet.WEETH);
+        steth = IERC20(Mainnet.STETH);
+        wsteth = IERC20(Mainnet.WSTETH);
+        lidoARM = LidoARM(payable(Mainnet.LIDO_ARM));
+        etherfiARM = EtherFiARM(payable(Mainnet.ETHERFI_ARM));
+    }
+
+    function _deployContracts() internal {
+        // Deploy Router
+        router = new ARMRouter(address(weth));
+
+        // Register ARMs in the Router
+        router.registerConfig(
+            address(steth), address(weth), ARMRouter.SwapType.ARM, address(lidoARM), bytes4(0), bytes4(0)
+        );
+        router.registerConfig(
+            address(weth), address(steth), ARMRouter.SwapType.ARM, address(lidoARM), bytes4(0), bytes4(0)
+        );
+        router.registerConfig(
+            address(eeth), address(weth), ARMRouter.SwapType.ARM, address(etherfiARM), bytes4(0), bytes4(0)
+        );
+        router.registerConfig(
+            address(weth), address(eeth), ARMRouter.SwapType.ARM, address(etherfiARM), bytes4(0), bytes4(0)
+        );
+
+        bytes4 wrapSelector = bytes4(keccak256("wrap(uint256)"));
+        bytes4 unwrapSelector = bytes4(keccak256("unwrap(uint256)"));
+        bytes4 getWstETHByStETH = bytes4(keccak256("getWstETHByStETH(uint256)"));
+        bytes4 getStETHByWstETH = bytes4(keccak256("getStETHByWstETH(uint256)"));
+        bytes4 getWeETHByeETH = bytes4(keccak256("getWeETHByeETH(uint256)"));
+        bytes4 getEETHByWeETH = bytes4(keccak256("getEETHByWeETH(uint256)"));
+        // Register wrappers in the Router
+        router.registerConfig(
+            address(steth), address(wsteth), ARMRouter.SwapType.WRAPPER, address(wsteth), wrapSelector, getWstETHByStETH
+        );
+        router.registerConfig(
+            address(wsteth),
+            address(steth),
+            ARMRouter.SwapType.WRAPPER,
+            address(wsteth),
+            unwrapSelector,
+            getStETHByWstETH
+        );
+        router.registerConfig(
+            address(eeth), address(weeth), ARMRouter.SwapType.WRAPPER, address(weeth), wrapSelector, getWeETHByeETH
+        );
+        router.registerConfig(
+            address(weeth), address(eeth), ARMRouter.SwapType.WRAPPER, address(weeth), unwrapSelector, getEETHByWeETH
+        );
+    }
+
+    function _fundContracts() internal {
+        // Fund test contract
+        deal(address(weth), address(this), 1_000 ether);
+        deal(address(weth), Mainnet.TREASURY_LP, 1_000 ether);
+        vm.prank(Mainnet.WSTETH);
+        steth.transfer(address(this), 1_000 ether);
+        vm.prank(Mainnet.WEETH);
+        eeth.transfer(address(this), 1_000 ether);
+
+        steth.approve(address(wsteth), type(uint256).max);
+        (bool success,) = address(wsteth).call(abi.encodeWithSignature("wrap(uint256)", 500 ether));
+        require(success, "Wrap WSTETH failed");
+        eeth.approve(address(weeth), type(uint256).max);
+        (success,) = address(weeth).call(abi.encodeWithSignature("wrap(uint256)", 500 ether));
+        require(success, "Wrap WEETH failed");
+
+        // Manage approvals
+        weth.approve(address(lidoARM), type(uint256).max);
+        weth.approve(address(etherfiARM), type(uint256).max);
+        eeth.approve(address(router), type(uint256).max);
+        eeth.approve(address(etherfiARM), type(uint256).max);
+        weeth.approve(address(router), type(uint256).max);
+        steth.approve(address(router), type(uint256).max);
+        steth.approve(address(lidoARM), type(uint256).max);
+        wsteth.approve(address(router), type(uint256).max);
+
+        // Deposit 100 WETH in Lido ARM
+        lidoARM.deposit(100 ether);
+
+        // Deposit 100 WETH in EtherFi ARM
+        vm.startPrank(Mainnet.TREASURY_LP);
+        weth.approve(address(etherfiARM), type(uint256).max);
+        etherfiARM.deposit(100 ether);
+        vm.stopPrank();
+
+        // Swap 50 STETH into WETH to fund the Lido ARM with STETH
+        lidoARM.swapExactTokensForTokens(IERC20(address(steth)), IERC20(address(weth)), 50 ether, 0, address(this));
+        // Swap 50 EETH into WETH to fund the EtherFi ARM with EETH
+        etherfiARM.swapExactTokensForTokens(IERC20(address(eeth)), IERC20(address(weth)), 50 ether, 0, address(this));
+    }
+
+    receive() external payable {}
+}
---
+++ test/fork/RouterARM/SwapExactTokensForTokens.t.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {Fork_Shared_ARMRouter_Test} from "./Shared.sol";
+
+contract Fork_Concrete_OriginARM_ARMRouter_Test_ is Fork_Shared_ARMRouter_Test {
+    function test_Swap_ExactTokensForTokens_EETH_WETH() public {
+        // Swap eeth to weth
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.startSnapshotGas("externalA");
+        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        vm.stopSnapshotGas();
+    }
+
+    function test_Swap_ExactTokensForTokens_WEETH_WETH () public {
+        // Swap weeth to weth
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](3);
+        path[0] = address(weeth);
+        path[1] = address(eeth);
+        path[2] = address(weth);
+
+        vm.startSnapshotGas("externalB");
+        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        vm.stopSnapshotGas();
+    }
+
+    function test_Swap_ExactTokensForTokens_WEETH_WSTETH() public {
+        // Swap weeth to wsteth
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](5);
+        path[0] = address(weeth);
+        path[1] = address(eeth);
+        path[2] = address(weth);
+        path[3] = address(steth);
+        path[4] = address(wsteth);
+
+        vm.startSnapshotGas("externalC");
+        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        vm.stopSnapshotGas();
+    }
+
+    function test_Swap_ExactETHForTokens() public {
+        // Swap eth to eeth
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(weth);
+        path[1] = address(eeth);
+
+        vm.startSnapshotGas("externalD");
+        router.swapExactETHForTokens{value: amountIn}(0, path, address(this), block.timestamp + 1);
+        vm.stopSnapshotGas();
+    }
+
+    function test_Swap_ExactTokensForETH() public {
+        // Swap eeth to eth
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.startSnapshotGas("externalE");
+        router.swapExactTokensForETH(amountIn, 0, path, address(this), block.timestamp + 1);
+        vm.stopSnapshotGas();
+    }
+}
