6d5a812946255e4cf889bed0c5d34d6357db2a05
Clément <clemmoller@gmail.com>
2025-11-03 13:24:28 +0100

    fmt

--- test/fork/RouterARM/SwapTokensForExactTokens.t.sol
+++ test/fork/RouterARM/SwapTokensForExactTokens.t.sol
@@ -3,35 +3,35 @@ pragma solidity 0.8.23;
 
 import {Fork_Shared_ARMRouter_Test} from "./Shared.sol";
 
-contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_ARMRouter_Test {
-    function test_Swap_ExactTokensForTokens_EETH_WETH() public {
+contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_ARMRouter_Test {
+    function test_Swap_TokensForExactTokens_EETH_WETH() public {
         // Swap eeth to weth
-        uint256 amountIn = 10 ether;
+        uint256 amountOut = 10 ether;
         address[] memory path = new address[](2);
         path[0] = address(eeth);
         path[1] = address(weth);
 
         vm.startSnapshotGas("externalA");
-        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
         vm.stopSnapshotGas();
     }
 
-    function test_Swap_ExactTokensForTokens_WEETH_WETH() public {
+    function test_Swap_TokensForExactTokens_WEETH_WETH() public {
         // Swap weeth to weth
-        uint256 amountIn = 10 ether;
+        uint256 amountOut = 10 ether;
         address[] memory path = new address[](3);
         path[0] = address(weeth);
         path[1] = address(eeth);
         path[2] = address(weth);
 
         vm.startSnapshotGas("externalB");
-        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
         vm.stopSnapshotGas();
     }
 
-    function test_Swap_ExactTokensForTokens_WEETH_WSTETH() public {
+    function test_Swap_TokensForExactTokens_WEETH_WSTETH() public {
         // Swap weeth to wsteth
-        uint256 amountIn = 10 ether;
+        uint256 amountOut = 10 ether;
         address[] memory path = new address[](5);
         path[0] = address(weeth);
         path[1] = address(eeth);
@@ -40,31 +40,31 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[4] = address(wsteth);
 
         vm.startSnapshotGas("externalC");
-        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
         vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactETHForTokens() public {
         // Swap eth to eeth
-        uint256 amountIn = 10 ether;
+        uint256 amountOut = 10 ether;
         address[] memory path = new address[](2);
         path[0] = address(weth);
         path[1] = address(eeth);
 
         vm.startSnapshotGas("externalD");
-        router.swapExactETHForTokens{value: amountIn}(0, path, address(this), block.timestamp + 1);
+        router.swapExactETHForTokens{value: amountOut}(type(uint256).max, path, address(this), block.timestamp + 1);
         vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactTokensForETH() public {
         // Swap eeth to eth
-        uint256 amountIn = 10 ether;
+        uint256 amountOut = 10 ether;
         address[] memory path = new address[](2);
         path[0] = address(eeth);
         path[1] = address(weth);
 
         vm.startSnapshotGas("externalE");
-        router.swapExactTokensForETH(amountIn, 0, path, address(this), block.timestamp + 1);
+        router.swapExactTokensForETH(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
         vm.stopSnapshotGas();
     }
 }
