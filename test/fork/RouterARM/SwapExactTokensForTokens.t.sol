fe5e0fc45c70e507e260455b8d3c1a8a4e2a00d2
Clément <clemmoller@gmail.com>
2025-11-04 15:35:22 +0100

    Add more fork test for ARMRouter

--- test/fork/RouterARM/SwapExactTokensForTokens.t.sol
+++ test/fork/RouterARM/SwapExactTokensForTokens.t.sol
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Fork_Shared_ARMRouter_Test} from "./Shared.sol";
-
+// Contracts
 import {ARMRouter} from "contracts/ARMRouter.sol";
 
+// Tests
+import {Fork_Shared_ARMRouter_Test} from "./Shared.sol";
+
 contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_ARMRouter_Test {
+    ////////////////////////////////////////////////////
+    ///                 Tests
+    ////////////////////////////////////////////////////
     function test_Swap_ExactTokensForTokens_EETH_WETH() public {
         // Swap eeth to weth
         uint256 amountIn = 10 ether;
@@ -13,9 +18,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[0] = address(eeth);
         path[1] = address(weth);
 
-        vm.startSnapshotGas("externalA");
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactTokensForTokens_WEETH_WETH() public {
@@ -26,9 +29,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[1] = address(eeth);
         path[2] = address(weth);
 
-        vm.startSnapshotGas("externalB");
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactTokensForTokens_WEETH_WSTETH() public {
@@ -41,9 +42,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[3] = address(steth);
         path[4] = address(wsteth);
 
-        vm.startSnapshotGas("externalC");
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactETHForTokens() public {
@@ -53,9 +52,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[0] = address(weth);
         path[1] = address(eeth);
 
-        vm.startSnapshotGas("externalD");
         router.swapExactETHForTokens{value: amountIn}(0, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_ExactTokensForETH() public {
@@ -65,12 +62,13 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         path[0] = address(eeth);
         path[1] = address(weth);
 
-        vm.startSnapshotGas("externalE");
         router.swapExactTokensForETH(amountIn, 0, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
-    function test_Revert_Because_Insufficient_Output() public {
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapExactTokensForTokens
+    ////////////////////////////////////////////////////
+    function test_Revert_When_SwapExactTokensForTokens_Because_Insufficient_Output() public {
         // Swap eeth to weth with high min amount out
         uint256 amountIn = 10 ether;
         address[] memory path = new address[](2);
@@ -81,7 +79,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         router.swapExactTokensForTokens(amountIn, 12 ether, path, address(this), block.timestamp + 1);
     }
 
-    function test_Revert_Because_Expired() public {
+    function test_Revert_When_SwapExactTokensForTokens_Because_Expired() public {
         // Swap eeth to weth with expired deadline
         uint256 amountIn = 10 ether;
         address[] memory path = new address[](2);
@@ -92,29 +90,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp - 1);
     }
 
-    function test_Revert_Because_InvalidePath_EthIn() public {
-        // Swap weth to eeth with invalid path
-        uint256 amountIn = 10 ether;
-        address[] memory path = new address[](2);
-        path[0] = address(weeth);
-        path[1] = address(eeth);
-
-        vm.expectRevert("ARMRouter: INVALID_PATH");
-        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
-    }
-
-    function test_Revert_Because_InvalidePath_EthOut() public {
-        // Swap eeth to weth with invalid path
-        uint256 amountIn = 10 ether;
-        address[] memory path = new address[](2);
-        path[0] = address(eeth);
-        path[1] = address(weeth);
-
-        vm.expectRevert("ARMRouter: INVALID_PATH");
-        router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
-    }
-
-    function test_Revert_Because_Wrap_Failed() public {
+    function test_Revert_When_SwapExactTokensForTokens_Because_Wrap_Failed() public {
         router.registerConfig(
             address(eeth),
             address(weeth),
@@ -134,7 +110,7 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
     }
 
-    function test_Revert_Because_PathNotFound() public {
+    function test_Revert_When_SwapExactTokensForTokens_Because_PathNotFound() public {
         // Swap eeth to weth with unregistered path
         uint256 amountIn = 10 ether;
         address[] memory path = new address[](2);
@@ -143,5 +119,57 @@ contract Fork_Concrete_ARMRouter_SwapExactTokensForTokens_Test_ is Fork_Shared_A
 
         vm.expectRevert("ARMRouter: PATH_NOT_FOUND");
         router.swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp + 1);
+    }
+
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapExactETHForTokens
+    ////////////////////////////////////////////////////
+
+    function test_Revert_When_SwapExactETHForTokens_Because_InvalidPath() public {
+        // Swap weth to eeth with invalid path
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(weeth);
+        path[1] = address(eeth);
+
+        vm.expectRevert("ARMRouter: INVALID_PATH");
+        router.swapExactETHForTokens{value: amountIn}(0, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapExactETHForTokens_Because_Insufficient_Output() public {
+        // Swap eth to eeth with high min amount out
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(weth);
+        path[1] = address(eeth);
+
+        vm.expectRevert("ARMRouter: INSUFFICIENT_OUTPUT");
+        router.swapExactETHForTokens{value: amountIn}(12 ether, path, address(this), block.timestamp + 1);
+    }
+
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapExactTokensForETH
+    ////////////////////////////////////////////////////
+
+    function test_Revert_When_SwapExactTokensForETH_Because_InvalidPath() public {
+        // Swap eeth to weth with invalid path
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weeth);
+
+        vm.expectRevert("ARMRouter: INVALID_PATH");
+        router.swapExactTokensForETH(amountIn, 0, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapExactTokensForETH_Because_Insufficient_Output() public {
+        // Swap eeth to eth with high min amount out
+        uint256 amountIn = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.expectRevert("ARMRouter: INSUFFICIENT_OUTPUT");
+        router.swapExactTokensForETH(amountIn, 12 ether, path, address(this), block.timestamp + 1);
     }
 }
--- test/fork/RouterARM/SwapTokensForExactTokens.t.sol
+++ test/fork/RouterARM/SwapTokensForExactTokens.t.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+// Contracts
+import {ARMRouter} from "contracts/ARMRouter.sol";
+
+// Tests
 import {Fork_Shared_ARMRouter_Test} from "./Shared.sol";
 
 contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_ARMRouter_Test {
+    ////////////////////////////////////////////////////
+    ///                 Tests
+    ////////////////////////////////////////////////////
     function test_Swap_TokensForExactTokens_EETH_WETH() public {
         // Swap eeth to weth
         uint256 amountOut = 10 ether;
@@ -11,9 +18,7 @@ contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_A
         path[0] = address(eeth);
         path[1] = address(weth);
 
-        vm.startSnapshotGas("externalA");
         router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_TokensForExactTokens_WEETH_WETH() public {
@@ -24,9 +29,7 @@ contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_A
         path[1] = address(eeth);
         path[2] = address(weth);
 
-        vm.startSnapshotGas("externalB");
         router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_TokensForExactTokens_WEETH_WSTETH() public {
@@ -39,9 +42,7 @@ contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_A
         path[3] = address(steth);
         path[4] = address(wsteth);
 
-        vm.startSnapshotGas("externalC");
         router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_ETHForExactTokens() public {
@@ -51,9 +52,7 @@ contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_A
         path[0] = address(weth);
         path[1] = address(eeth);
 
-        vm.startSnapshotGas("externalD");
         router.swapETHForExactTokens{value: amountOut * 2}(amountOut, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
     }
 
     function test_Swap_TokensForExactETH() public {
@@ -63,8 +62,112 @@ contract Fork_Concrete_ARMRouter_SwapTokensForExactTokens_Test_ is Fork_Shared_A
         path[0] = address(eeth);
         path[1] = address(weth);
 
-        vm.startSnapshotGas("externalE");
         router.swapTokensForExactETH(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
-        vm.stopSnapshotGas();
+    }
+
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapTokensForExactTokens
+    ////////////////////////////////////////////////////
+    function test_Revert_When_SwapTokensForExactTokens_Because_ExcessiveInput() public {
+        // Swap eeth to weth
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.expectRevert("ARMRouter: EXCESSIVE_INPUT");
+        router.swapTokensForExactTokens(amountOut, 5 ether, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapTokensForExactTokens_Because_Expired() public {
+        // Swap eeth to weth
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.expectRevert("ARMRouter: EXPIRED");
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp - 1);
+    }
+
+    function test_Revert_When_SwapTokensForExactTokens_Because_WrapFailed() public {
+        router.registerConfig(
+            address(eeth),
+            address(weeth),
+            ARMRouter.SwapType.WRAPPER,
+            address(weeth),
+            bytes4(0xffffffff),
+            bytes4(0xffffffff)
+        );
+
+        // Swap eeth to weeth
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weeth);
+
+        vm.expectRevert("ARMRouter: WRAP_FAILED");
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapTokensForExactTokens_Because_NotPathFound() public {
+        // Swap eeth to weeth without registering config
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(steth);
+
+        vm.expectRevert("ARMRouter: PATH_NOT_FOUND");
+        router.swapTokensForExactTokens(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
+    }
+
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapETHForExactTokens
+    ////////////////////////////////////////////////////
+    function test_Revert_When_SwapETHForExactTokens_Because_ExcessiveInput() public {
+        // Swap eth to eeth
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(weth);
+        path[1] = address(eeth);
+
+        vm.expectRevert("ARMRouter: EXCESSIVE_INPUT");
+        router.swapETHForExactTokens{value: 5 ether}(amountOut, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapETHForExactTokens_Because_InvalidPath() public {
+        // Swap weth to eeth with invalid path
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.expectRevert("ARMRouter: INVALID_PATH");
+        router.swapETHForExactTokens{value: amountOut * 2}(amountOut, path, address(this), block.timestamp + 1);
+    }
+
+    ////////////////////////////////////////////////////
+    ///    Revert Tests - SwapTokensForExactETH
+    ////////////////////////////////////////////////////
+    function test_Revert_When_SwapTokensForExactETH_Because_ExcessiveInput() public {
+        // Swap eeth to eth
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weth);
+
+        vm.expectRevert("ARMRouter: EXCESSIVE_INPUT");
+        router.swapTokensForExactETH(amountOut, 5 ether, path, address(this), block.timestamp + 1);
+    }
+
+    function test_Revert_When_SwapTokensForExactETH_Because_InvalidPath() public {
+        // Swap eeth to weth with invalid path
+        uint256 amountOut = 10 ether;
+        address[] memory path = new address[](2);
+        path[0] = address(eeth);
+        path[1] = address(weeth);
+
+        vm.expectRevert("ARMRouter: INVALID_PATH");
+        router.swapTokensForExactETH(amountOut, type(uint256).max, path, address(this), block.timestamp + 1);
     }
 }
