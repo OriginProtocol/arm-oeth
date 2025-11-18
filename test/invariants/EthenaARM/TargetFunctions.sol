2f90f7a4878f3be477bd77d7a6f5d279b57d363d
Clément <clemmoller@gmail.com>
2025-11-18 09:12:05 +0100

    Prepare list for target functions

--- test/invariants/EthenaARM/TargetFunctions.sol
+++ test/invariants/EthenaARM/TargetFunctions.sol
@@ -7,4 +7,39 @@ import {Setup} from "./Setup.sol";
 /// @title TargetFunctions
 /// @notice TargetFunctions contract for tests, containing the target functions that should be tested.
 ///         This is the entry point with the contract we are testing. Ideally, it should never revert.
-abstract contract TargetFunctions is Setup {}
+abstract contract TargetFunctions is Setup {
+    // ╔══════════════════════════════════════════════════════════════════════════════╗
+    // ║                              ✦✦✦ ETHENA ARM ✦✦✦                              ║
+    // ╚══════════════════════════════════════════════════════════════════════════════╝
+    // [ ] SwapExactTokensForTokens
+    // [ ] SwapTokensForExactTokens
+    // [ ] Deposit
+    // [ ] Allocate
+    // [ ] CollectFees
+    // [ ] RequestRedeem
+    // [ ] ClaimRedeem
+    // [ ] RequestBaseWithdrawal
+    // [ ] ClaimBaseWithdrawals
+    // --- Admin functions
+    // [ ] SetPrices
+    // [ ] SetCrossPrice
+    // [ ] SetFee
+    // [ ] SetActiveMarket
+    // [ ] SetARMBuffer
+    //
+    // ╔══════════════════════════════════════════════════════════════════════════════╗
+    // ║                                ✦✦✦ SUSDE ✦✦✦                                 ║
+    // ╚══════════════════════════════════════════════════════════════════════════════╝
+    // [ ] Deposit
+    // [ ] CoolDownShares
+    // [ ] Unstake
+    // [ ] TransferInRewards
+    //
+    // ╔══════════════════════════════════════════════════════════════════════════════╗
+    // ║                                ✦✦✦ MORPHO ✦✦✦                                ║
+    // ╚══════════════════════════════════════════════════════════════════════════════╝
+    // [ ] Deposit
+    // [ ] Withdraw
+    // [ ] TransferInRewards
+
+    }
