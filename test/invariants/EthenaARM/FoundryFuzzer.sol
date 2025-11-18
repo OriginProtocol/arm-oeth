20cf9aabc271e7d10fbe6b45f52b9ca285f2fc6f
Clément <clemmoller@gmail.com>
2025-11-18 11:56:57 +0100

    wip add targets for susde contract

--- test/invariants/EthenaARM/FoundryFuzzer.sol
+++ test/invariants/EthenaARM/FoundryFuzzer.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.23;
 
 // Test imports
 import {Properties} from "./Properties.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 /// @title FuzzerFoundry
 /// @notice Concrete fuzzing contract implementing Foundry's invariant testing framework.
@@ -12,8 +13,33 @@ import {Properties} from "./Properties.sol";
 ///         - Implements invariant test functions that call property validators
 ///         - Each invariant function represents a critical system property to maintain
 ///         - Fuzzer will call targeted handlers randomly and check invariants after each call
-contract FuzzerFoundry_EthenaARM is Properties {
+contract FuzzerFoundry_EthenaARM is Properties, StdInvariant {
     bool public constant override isLabelAvailable = true;
+    bool public constant override isAssumeAvailable = true;
 
-    function test() public {}
+    //////////////////////////////////////////////////////
+    /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public override {
+        super.setUp();
+
+        // --- Setup Fuzzer target ---
+        // Setup target
+        targetContract(address(this));
+
+        // Add selectors
+        bytes4[] memory selectors = new bytes4[](4);
+        selectors[0] = this.targetSUSDeDeposit.selector;
+        selectors[1] = this.targetSUSDeCooldownShares.selector;
+        selectors[2] = this.targetSUSDeUnstake.selector;
+        selectors[3] = this.targetSUSDeTransferInRewards.selector;
+
+        // Target selectors
+        targetSelector(FuzzSelector({addr: address(this), selectors: selectors}));
+    }
+
+    //////////////////////////////////////////////////////
+    /// --- INVARIANTS
+    //////////////////////////////////////////////////////
+    function invariantA() public {}
 }
--- test/invariants/EthenaARM/TargetFunctions.sol
+++ test/invariants/EthenaARM/TargetFunctions.sol
@@ -3,11 +3,18 @@ pragma solidity 0.8.23;
 
 // Test imports
 import {Setup} from "./Setup.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+
+// Solmate
+import {MockERC20} from "@solmate/test/utils/mocks/MockERC20.sol";
+
+// Contracts
+import {UserCooldown} from "contracts/Interfaces.sol";
 
 /// @title TargetFunctions
 /// @notice TargetFunctions contract for tests, containing the target functions that should be tested.
 ///         This is the entry point with the contract we are testing. Ideally, it should never revert.
-abstract contract TargetFunctions is Setup {
+abstract contract TargetFunctions is Setup, StdUtils {
     // ╔══════════════════════════════════════════════════════════════════════════════╗
     // ║                              ✦✦✦ ETHENA ARM ✦✦✦                              ║
     // ╚══════════════════════════════════════════════════════════════════════════════╝
@@ -33,6 +40,7 @@ abstract contract TargetFunctions is Setup {
     // [ ] Deposit
     // [ ] CoolDownShares
     // [ ] Unstake
+    // --- Admin functions
     // [ ] TransferInRewards
     //
     // ╔══════════════════════════════════════════════════════════════════════════════╗
@@ -41,5 +49,75 @@ abstract contract TargetFunctions is Setup {
     // [ ] Deposit
     // [ ] Withdraw
     // [ ] TransferInRewards
+    // ╔══════════════════════════════════════════════════════════════════════════════╗
+    // ║                                   ✦✦✦  ✦✦✦                                   ║
+    // ╚══════════════════════════════════════════════════════════════════════════════╝
 
+    // ╔══════════════════════════════════════════════════════════════════════════════╗
+    // ║                                ✦✦✦ SUSDE ✦✦✦                                 ║
+    // ╚══════════════════════════════════════════════════════════════════════════════╝
+    function targetSUSDeDeposit(uint88 amount) external {
+        // Ensure we don't mint 0 shares.
+        uint256 totalAssets = susde.totalAssets();
+        uint256 totalSupply = susde.totalSupply();
+        uint256 minAmount = totalAssets / totalSupply + 1;
+        // Prevent zero deposits
+        amount = uint88(_bound(amount, minAmount, type(uint88).max));
+
+        // Mint amount to grace
+        MockERC20(address(usde)).mint(grace, amount);
+
+        // Deposit as grace
+        vm.prank(grace);
+        susde.deposit(amount, grace);
     }
+
+    function targetSUSDeCooldownShares(uint88 shareAmount) external {
+        // Cache balance
+        uint256 balance = susde.balanceOf(grace);
+
+        // Assume balance not zero
+        if (assume(balance > 1)) return;
+
+        // Bound shareAmount to [1, balance]
+        shareAmount = uint88(_bound(shareAmount, 1, balance));
+
+        // Cooldown shares as grace
+        vm.prank(grace);
+        susde.cooldownShares(shareAmount);
+    }
+
+    function targetSUSDeUnstake() external {
+        // Check grace's cooldown
+        UserCooldown memory cooldown = susde.cooldowns(grace);
+
+        // Ensure grace has a valid cooldown
+        if (assume(cooldown.cooldownEnd != 0)) return;
+
+        // Fast forward to after cooldown end
+        vm.warp(cooldown.cooldownEnd + 1);
+
+        // Unstake as grace
+        vm.prank(grace);
+        susde.unstake(grace);
+
+        MockERC20(address(usde)).burn(grace, cooldown.underlyingAmount);
+    }
+
+    function targetSUSDeTransferInRewards(uint8 bps) external {
+        // Ensure enough time has passed since last distribution
+        uint256 lastDistribution = susde.lastDistributionTimestamp();
+        if (block.timestamp - lastDistribution < 8 hours) {
+            // Fast forward time to allow rewards distribution
+            vm.warp(lastDistribution + 8 hours + 1);
+        }
+
+        uint256 balance = usde.balanceOf(address(susde));
+        // Rewards can be distributed 3/days max. 1bps at every distribution -> 10 APR.
+        bps = uint8(_bound(bps, 1, 10));
+        uint256 rewards = (balance * bps) / 10_000;
+        MockERC20(address(usde)).mint(governor, rewards);
+        vm.prank(governor);
+        susde.transferInRewards(rewards);
+    }
+}
