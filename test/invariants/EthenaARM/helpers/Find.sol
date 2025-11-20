5b7e924866ceee7cf8c369bd5dcac5c93fc11e1d
Clément <clemmoller@gmail.com>
2025-11-20 17:40:42 +0100

    Fix ClaimRequest checks

--- test/invariants/EthenaARM/helpers/Find.sol
+++ test/invariants/EthenaARM/helpers/Find.sol
@@ -11,25 +11,26 @@ library Find {
         uint248 randomAddressIndex;
         uint248 randomArrayIndex;
         address[] users;
-        uint256 targetAmount;
+        uint128 claimable;
+        uint128 availableLiquidity;
     }
 
     function getUserRequestWithAmount(
         GetUserRequestWithAmountStruct memory $,
         mapping(address => uint256[]) storage pendingRequests
     ) internal returns (address user, uint256 requestId, uint40 claimTimestamp) {
-        uint256 usersLen = $.users.length;
-        for (uint256 i; i < usersLen; i++) {
+        for (uint256 i; i < $.users.length; i++) {
             // Take a random user
-            address _user = $.users[($.randomAddressIndex + i) % usersLen];
+            address _user = $.users[($.randomAddressIndex + i) % $.users.length];
             // Find a request that can be claimed
             for (uint256 j; j < pendingRequests[_user].length; j++) {
                 // Take a random request from that user
                 uint256 _requestId = pendingRequests[_user][($.randomArrayIndex + j) % pendingRequests[_user].length];
                 // Check request data
-                (,, uint40 _claimTimestamp,, uint128 _queued) = AbstractARM($.arm).withdrawalRequests(_requestId);
+                (,, uint40 _claimTimestamp, uint128 _amount, uint128 _queued) =
+                    AbstractARM($.arm).withdrawalRequests(_requestId);
                 // Check if this is claimable
-                if (_queued <= $.targetAmount) {
+                if (_queued <= $.claimable && _amount <= $.availableLiquidity) {
                     (user, requestId, claimTimestamp) = (_user, _requestId, _claimTimestamp);
                     // Remove pendingRequests
                     pendingRequests[_user][($.randomArrayIndex + j) % pendingRequests[_user].length] =
