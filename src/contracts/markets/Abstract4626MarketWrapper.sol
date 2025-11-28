de744dffc44dc10422a81134d1ebdfd9aad4d9c0
Nick Addison <nick@addisonbrown.com.au>
2025-11-28 21:54:06 +1100

    Gas optimization in Abstract4626MarketWrapper's transferTokens (#164)

--- src/contracts/markets/Abstract4626MarketWrapper.sol
+++ src/contracts/markets/Abstract4626MarketWrapper.sol
@@ -238,7 +238,7 @@ contract Abstract4626MarketWrapper is Initializable, Ownable {
     function transferTokens(address token, address to, uint256 amount) external onlyOwner {
         require(token != market, "Cannot transfer market token");
         require(to != address(0), "Invalid address");
-        require(to == owner() || to == harvester, "Cannot transfer to non-owner or non-harvester");
+        require(to == msg.sender || to == harvester, "Cannot transfer to non-owner or non-harvester");
 
         if (address(token) == address(0)) {
             // Move ETH
