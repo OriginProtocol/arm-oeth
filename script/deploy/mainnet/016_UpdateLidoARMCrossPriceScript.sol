bc1fc2797299a14cd92cf4609ae4b7b699968f17
Nicholas Addison <nick@addisonbrown.com.au>
2026-01-15 09:22:12 +1100

    Marked deploy 016 as executed

--- script/deploy/mainnet/016_UpdateLidoARMCrossPriceScript.sol
+++ script/deploy/mainnet/016_UpdateLidoARMCrossPriceScript.sol
@@ -19,7 +19,7 @@ contract UpgradeLidoARMCrossPriceScript is AbstractDeployScript {
     GovProposal public govProposal;
 
     string public constant override DEPLOY_NAME = "016_UpgradeLidoARMCrossPriceScript";
-    bool public constant override proposalExecuted = false;
+    bool public constant override proposalExecuted = true;
 
     function _execute() internal override {
         console.log("Deploy:", DEPLOY_NAME);
