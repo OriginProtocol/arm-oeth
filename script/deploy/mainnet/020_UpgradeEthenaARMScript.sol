aabcd4bf937b5341b731583e1a47c8d1d126e360
Clément <clemmoller@gmail.com>
2026-02-23 09:15:40 +0100

    [Refactor] Simplify UpgradeEthenaARMScript by removing unused imports and logging

--- script/deploy/mainnet/020_UpgradeEthenaARMScript.sol
+++ script/deploy/mainnet/020_UpgradeEthenaARMScript.sol
@@ -1,49 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-// Foundry imports
-import {console} from "forge-std/console.sol";
-
 // Contract imports
 import {Proxy} from "contracts/Proxy.sol";
 import {EthenaARM} from "contracts/EthenaARM.sol";
 import {Mainnet} from "contracts/utils/Addresses.sol";
 
-// Deployment imports
-import {GovProposal, GovSixHelper} from "contracts/utils/GovSixHelper.sol";
-import {AbstractDeployScript} from "../AbstractDeployScript.sol";
+// Deployment
+import {AbstractDeployScript} from "script/deploy/helpers/AbstractDeployScript.s.sol";
 
-contract UpgradeEthenaARMScript2 is AbstractDeployScript {
-    using GovSixHelper for GovProposal;
-
-    GovProposal public govProposal;
-
-    string public constant override DEPLOY_NAME = "020_UpgradeEthenaARMScript";
-    bool public constant override proposalExecuted = false;
-
+contract $020_UpgradeEthenaARMScript is AbstractDeployScript("020_UpgradeEthenaARMScript") {
     EthenaARM armImpl;
 
     function _execute() internal override {
-        console.log("Deploy:", DEPLOY_NAME);
-        console.log("------------");
-
         // 1. Deploy new ARM implementation
-        uint256 claimDelay = tenderlyTestnet ? 1 minutes : 10 minutes;
         armImpl = new EthenaARM(
             Mainnet.USDE,
             Mainnet.SUSDE,
-            claimDelay,
+            10 minutes, // claimDelay
             1e18, // minSharesToRedeem
             100e18 // allocateThreshold
         );
-        _recordDeploy("ETHENA_ARM_IMPL", address(armImpl));
-
-        console.log("Finished deploying", DEPLOY_NAME);
+        _recordDeployment("ETHENA_ARM_IMPL", address(armImpl));
     }
 
     function _fork() internal override {
-        vm.startPrank(Proxy(payable(deployedContracts["ETHENA_ARM"])).owner());
-        Proxy(payable(deployedContracts["ETHENA_ARM"])).upgradeTo(address(armImpl));
+        vm.startPrank(Proxy(payable(resolver.resolve("ETHENA_ARM"))).owner());
+        Proxy(payable(resolver.resolve("ETHENA_ARM"))).upgradeTo(address(armImpl));
         vm.stopPrank();
     }
 }
