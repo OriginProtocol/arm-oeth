1ea820730bae44c4d5c6809a66e18fdaa019521c
Nicholas Addison <nick@addisonbrown.com.au>
2026-05-05 17:09:00 +1000

    Increased Ethena price offset

--- src/js/tasks/actions/setPricesEthena.ts
+++ src/js/tasks/actions/setPricesEthena.ts
@@ -24,7 +24,7 @@ action({
       amount: 2000,
       tolerance: 0.1,
       fee: 1,
-      offset: 0.2,
+      offset: 0.3,
       priceOffset: true,
       blockTag: "latest",
       wrapped: true,
