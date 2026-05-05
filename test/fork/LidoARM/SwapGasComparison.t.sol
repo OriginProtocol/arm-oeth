53101a172808cdabef1218b123f20c507b17846b
Nicholas Addison <nick@addisonbrown.com.au>
2026-05-05 12:48:54 +1000

    Update swap gas compare test

--- test/fork/LidoARM/SwapGasComparison.t.sol
+++ test/fork/LidoARM/SwapGasComparison.t.sol
@@ -6,26 +6,25 @@ import {Test} from "forge-std/Test.sol";
 import {LidoARM} from "contracts/LidoARM.sol";
 import {Proxy} from "contracts/Proxy.sol";
 import {IERC20} from "contracts/Interfaces.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {Mainnet} from "contracts/utils/Addresses.sol";
 
-contract Fork_Concrete_LidoARM_SwapGasComparison_Test is Test {
+abstract contract Fork_LidoARM_SwapGasComparison_Base is Test {
     uint256 internal constant FORK_BLOCK = 24_846_066;
     uint256 internal constant PRICE_SCALE = 1e36;
+    uint256 internal constant LIQUIDITY_DEPOSIT = 1_000 ether;
+    uint256 internal constant SWAP_INPUT = 100 ether;
+    uint256 internal constant SWAP_OUTPUT = 100 ether;
 
     LidoARM internal lidoARM;
     Proxy internal lidoProxy;
     IERC20 internal weth;
     IERC20 internal steth;
 
-    uint256 internal reserveWeth;
+    uint256 internal traderate0;
     uint256 internal traderate1;
-    address internal activeMarket;
-    uint256 internal marketLiquidity;
     uint256 internal amountInEnoughLiquidity;
-    uint256 internal amountInNeedsMarketLiquidity;
 
-    function setUp() public {
+    function _setUpMainnetForkWithLiquidity() internal {
         vm.createSelectFork(vm.envString("MAINNET_URL"), FORK_BLOCK);
 
         lidoARM = LidoARM(payable(Mainnet.LIDO_ARM));
@@ -33,55 +32,85 @@ contract Fork_Concrete_LidoARM_SwapGasComparison_Test is Test {
         weth = IERC20(Mainnet.WETH);
         steth = IERC20(Mainnet.STETH);
 
-        (reserveWeth,) = lidoARM.getReserves();
+        traderate0 = lidoARM.traderate0();
         traderate1 = lidoARM.traderate1();
-        activeMarket = lidoARM.activeMarket();
-        marketLiquidity = IERC4626(activeMarket).maxWithdraw(address(lidoARM));
 
-        require(activeMarket != address(0), "missing active market");
-        require(reserveWeth > 0, "missing ARM liquidity");
-        require(marketLiquidity > 0, "missing market liquidity");
+        deal(address(weth), address(this), LIQUIDITY_DEPOSIT);
+        weth.approve(address(lidoARM), LIQUIDITY_DEPOSIT);
+        lidoARM.deposit(LIQUIDITY_DEPOSIT);
 
-        amountInEnoughLiquidity = _amountInForDesiredOut(reserveWeth / 10);
-        amountInNeedsMarketLiquidity = _amountInForDesiredOut(reserveWeth + _marketShortfallTarget());
+        amountInEnoughLiquidity = _amountInForDesiredOut(SWAP_OUTPUT);
 
-        _fundSteth(amountInNeedsMarketLiquidity);
-        steth.approve(address(lidoARM), type(uint256).max);
+        _fundSteth(amountInEnoughLiquidity);
+        steth.approve(address(lidoARM), amountInEnoughLiquidity);
     }
 
-    function test_Gas_UpgradedArm_EnoughLiquidity() public {
-        _upgradeLidoArm();
+    function _measureSwap(uint256 amountIn) internal returns (uint256 gasUsed, uint256 amountOut) {
+        uint256 gasBefore = gasleft();
+        uint256[] memory amounts = lidoARM.swapExactTokensForTokens(steth, weth, amountIn, 0, address(this));
+        gasUsed = gasBefore - gasleft();
+        amountOut = amounts[1];
+    }
 
-        (uint256 gasUsed, uint256 amountOut) = _measureSwap(amountInEnoughLiquidity);
+    function _amountInForDesiredOut(uint256 desiredOut) internal view returns (uint256) {
+        return desiredOut * PRICE_SCALE / traderate1 + 1;
+    }
 
-        emit log_named_uint("fork_block", block.number);
-        emit log_named_uint("amount_in_stETH", amountInEnoughLiquidity);
-        emit log_named_uint("amount_out_WETH", amountOut);
-        emit log_named_uint("gas_used", gasUsed);
+    function _fundSteth(uint256 amount) internal {
+        vm.prank(Mainnet.WSTETH);
+        steth.transfer(address(this), amount);
+    }
+}
+
+contract Fork_Concrete_LidoARM_SwapGasCurrentDeployed_Test is Fork_LidoARM_SwapGasComparison_Base {
+    function setUp() public {
+        _setUpMainnetForkWithLiquidity();
     }
 
     function test_Gas_CurrentDeployedArm_EnoughLiquidity() public {
         (uint256 gasUsed, uint256 amountOut) = _measureSwap(amountInEnoughLiquidity);
 
         emit log_named_uint("fork_block", block.number);
+        emit log_named_uint("liquidity_deposit_WETH", LIQUIDITY_DEPOSIT);
         emit log_named_uint("amount_in_stETH", amountInEnoughLiquidity);
         emit log_named_uint("amount_out_WETH", amountOut);
         emit log_named_uint("gas_used", gasUsed);
     }
 
-    function test_Gas_UpgradedArm_NeedsMarketLiquidity() public {
-        _upgradeLidoArm();
-
-        uint256 reserveBefore = reserveWeth;
-        (uint256 gasUsed, uint256 amountOut) = _measureSwap(amountInNeedsMarketLiquidity);
-
-        require(amountOut > reserveBefore, "swap did not need market liquidity");
+    function test_Gas_CurrentDeployedArm_ExactInput() public {
+        (uint256 gasUsed, uint256 amountOut) = _measureSwap(SWAP_INPUT);
 
         emit log_named_uint("fork_block", block.number);
-        emit log_named_uint("amount_in_stETH", amountInNeedsMarketLiquidity);
+        emit log_named_uint("liquidity_deposit_WETH", LIQUIDITY_DEPOSIT);
+        emit log_named_uint("amount_in_stETH", SWAP_INPUT);
         emit log_named_uint("amount_out_WETH", amountOut);
-        emit log_named_uint("weth_reserve_before", reserveBefore);
-        emit log_named_uint("market_shortfall", amountOut - reserveBefore);
+        emit log_named_uint("gas_used", gasUsed);
+    }
+}
+
+contract Fork_Concrete_LidoARM_SwapGasUpgraded_Test is Fork_LidoARM_SwapGasComparison_Base {
+    function setUp() public {
+        _setUpMainnetForkWithLiquidity();
+        _upgradeLidoArm();
+    }
+
+    function test_Gas_UpgradedArm_EnoughLiquidity() public {
+        (uint256 gasUsed, uint256 amountOut) = _measureSwap(amountInEnoughLiquidity);
+
+        emit log_named_uint("fork_block", block.number);
+        emit log_named_uint("liquidity_deposit_WETH", LIQUIDITY_DEPOSIT);
+        emit log_named_uint("amount_in_stETH", amountInEnoughLiquidity);
+        emit log_named_uint("amount_out_WETH", amountOut);
+        emit log_named_uint("gas_used", gasUsed);
+    }
+
+    function test_Gas_UpgradedArm_ExactInput() public {
+        (uint256 gasUsed, uint256 amountOut) = _measureSwap(SWAP_INPUT);
+
+        emit log_named_uint("fork_block", block.number);
+        emit log_named_uint("liquidity_deposit_WETH", LIQUIDITY_DEPOSIT);
+        emit log_named_uint("amount_in_stETH", SWAP_INPUT);
+        emit log_named_uint("amount_out_WETH", amountOut);
         emit log_named_uint("gas_used", gasUsed);
     }
 
@@ -97,28 +126,10 @@ contract Fork_Concrete_LidoARM_SwapGasComparison_Test is Test {
 
         vm.prank(lidoProxy.owner());
         lidoProxy.upgradeTo(address(upgradedImpl));
-    }
 
-    function _measureSwap(uint256 amountIn) internal returns (uint256 gasUsed, uint256 amountOut) {
-        uint256 gasBefore = gasleft();
-        uint256[] memory amounts = lidoARM.swapExactTokensForTokens(steth, weth, amountIn, 0, address(this));
-        gasUsed = gasBefore - gasleft();
-        amountOut = amounts[1];
-    }
+        uint256 sellT1 = PRICE_SCALE * PRICE_SCALE / traderate0;
 
-    function _amountInForDesiredOut(uint256 desiredOut) internal view returns (uint256) {
-        return desiredOut * PRICE_SCALE / traderate1 + 1;
-    }
-
-    function _marketShortfallTarget() internal view returns (uint256 shortfall) {
-        shortfall = marketLiquidity / 100;
-        if (shortfall > 25 ether) shortfall = 25 ether;
-        if (shortfall == 0) shortfall = 1 ether;
-        require(shortfall < marketLiquidity, "insufficient market liquidity");
-    }
-
-    function _fundSteth(uint256 amount) internal {
-        vm.prank(Mainnet.WSTETH);
-        steth.transfer(address(this), amount);
+        vm.prank(lidoProxy.owner());
+        lidoARM.setPrices(traderate1, sellT1, type(uint256).max, type(uint256).max);
     }
 }
