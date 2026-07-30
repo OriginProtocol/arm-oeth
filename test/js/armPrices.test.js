3c148515370369f1a4359eb9c15b8d2633d8d0e7
Nicholas Addison <nick@addisonbrown.com.au>
2026-07-30 16:06:51 +1000

    fix: cap automatic DEX quote amounts by available liquidity

--- test/js/armPrices.test.js
+++ test/js/armPrices.test.js
@@ -1,6 +1,10 @@
 const assert = require("assert");
 
-const { haveSwapCapsChanged } = require("../../src/js/utils/priceUpdate");
+const {
+  capDexAmountBySwapLiquidity,
+  haveSwapCapsChanged,
+  resolveDexQuoteAmount,
+} = require("../../src/js/utils/priceUpdate");
 
 const multiBaseContext = (buyLiquidityRemaining, sellLiquidityRemaining) => ({
   version: "multiBase",
@@ -32,6 +36,81 @@ assert.strictEqual(
   haveSwapCapsChanged({ version: "legacy" }, 10n, 20n),
   false,
   "legacy ARMs do not support buy and sell amounts",
+);
+
+assert.strictEqual(
+  capDexAmountBySwapLiquidity({
+    amount: 100,
+    buyLiquidity: 50000000n,
+    sellLiquidity: (1n << 128n) - 1n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "50.0",
+  "buy quote amount should not exceed buy liquidity",
+);
+
+assert.strictEqual(
+  capDexAmountBySwapLiquidity({
+    amount: 100,
+    buyLiquidity: (1n << 128n) - 1n,
+    sellLiquidity: 25000000000000000000n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "25.0",
+  "sell quote amount should not exceed sell liquidity",
+);
+
+assert.strictEqual(
+  capDexAmountBySwapLiquidity({
+    amount: 20,
+    buyLiquidity: 30000000n,
+    sellLiquidity: 10000000000000000000n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "10.0",
+  "quote amount should use the lower of buy and sell liquidity",
+);
+
+assert.strictEqual(
+  capDexAmountBySwapLiquidity({
+    amount: 20,
+    buyLiquidity: 30000000n,
+    sellLiquidity: 40000000000000000000n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "20",
+  "quote amount within both liquidity limits should be unchanged",
+);
+
+assert.strictEqual(
+  resolveDexQuoteAmount({
+    amount: 100,
+    liquidityAssets: 50000000n,
+    baseAssetReserve: 25000000000000000000n,
+    buyLiquidity: 30000000n,
+    sellLiquidity: 10000000000000000000n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "100",
+  "an explicit quote amount should override reserves and price liquidity",
+);
+
+assert.strictEqual(
+  resolveDexQuoteAmount({
+    liquidityAssets: 50000000n,
+    baseAssetReserve: 40000000000000000000n,
+    buyLiquidity: 30000000n,
+    sellLiquidity: 20000000000000000000n,
+    liquidityDecimals: 6,
+    baseDecimals: 18,
+  }),
+  "20.0",
+  "an automatic quote amount should use the smallest reserve or price liquidity limit",
 );
 
 console.log("ARM price tests passed");
