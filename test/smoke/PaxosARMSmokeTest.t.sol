db7a6e56922310070a790b9aaaa2e32ae394c552
Nicholas Addison <nick@addisonbrown.com.au>
2026-08-21 16:56:53 +1000

    Fix smoke test

--- test/smoke/PaxosARMSmokeTest.t.sol
+++ test/smoke/PaxosARMSmokeTest.t.sol
@@ -75,7 +75,7 @@ contract Fork_PaxosARM_Smoke_Test is AbstractSmokeTest {
         _assertBaseAssetListed(baseAssets, Mainnet.USDG, "USDG listed as base asset");
 
         assertEq(capManager.arm(), address(usdcARM), "cap manager arm");
-        assertEq(capManager.totalAssetsCap(), 1_000_000e18, "total assets cap");
+        assertEq(capManager.totalAssetsCap(), 500_000e6, "total assets cap");
         assertEq(capManager.accountCapEnabled(), true, "account cap enabled");
         assertEq(
             capManager.liquidityProviderCaps(Mainnet.TREASURY_LP), 1_000_000e18 - 200_000e6, "liquidity provider cap"
