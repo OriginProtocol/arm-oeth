5320d7711bee29190af97aedf0b5d8bc3996b2ab
Nicholas Addison <nick@addisonbrown.com.au>
2026-08-24 14:45:53 +1000

    Fix compilation

--- test/unit/markets/MorphoBlueMarket.t.sol
+++ test/unit/markets/MorphoBlueMarket.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.36;
 
 import {Test} from "forge-std/Test.sol";
 import {MockERC20} from "@solmate/test/utils/mocks/MockERC20.sol";
